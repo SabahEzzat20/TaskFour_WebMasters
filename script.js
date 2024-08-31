a257d7e7860f44bac8e2263030d306b596c1e901
SabahHassan692 <103220823+SabahHassan692@users.noreply.github.com>
2024-08-31 03:54:36 +0300

    finish task 4 with dropdown menus for additional improvements for ux or design requirement and adding some helpful comments

--- script.js
+++ script.js
@@ -1,7 +1,74 @@
 let backToTopButton = document.getElementById('back-top-btn');
 let menuBar = document.getElementsByClassName('menu-icon')[0];
 let menu = document.querySelector('.menu');
-console.log(menu);
+let linksMenus = document.querySelectorAll('.link-menu');
+let dropdownMenus = document.querySelectorAll('.dropdown-menu');
+let chevron = document.querySelectorAll('.link-menu i')
+
+
+
+// aside dropdown menus functionality
+
+chevron.forEach(function (arrow,index) {
+    arrow.setAttribute('data-ind', index);
+})
+
+
+dropdownMenus.forEach(function (dropMenu,i) {
+    dropMenu.setAttribute('data-i', i);
+    console.log(dropMenu.getAttribute('data-i'));
+})
+
+
+linksMenus.forEach(function (menu,index) {
+    menu.setAttribute('data-index', index);
+    menu.addEventListener('click', function (e) {
+            showMenu(menu.getAttribute('data-index'));
+    })
+})
+
+
+function showMenu(i) {
+    dropdownMenus.forEach((dropMenu) => {
+        // Only toggle the dropdown menu that matches the clicked link menu
+        if (dropMenu.getAttribute('data-i') === i) {
+            if (dropMenu.style.display === 'block') {
+                dropMenu.style.display = 'none';
+                unRotateArrow(i);
+            } else {
+                dropMenu.style.display = 'block';
+                rotateArrow(i);
+            }
+        } else {
+            dropMenu.style.display = 'none';
+            unRotateArrow(dropMenu.getAttribute('data-i'));
+        }
+    });
+}
+
+
+function rotateArrow(i) {
+    chevron.forEach(function (arrow) {
+        if (arrow.getAttribute('data-ind') === i) {
+            arrow.classList.add('rotated-arrow');
+        } else {
+            arrow.classList.remove('rotated-arrow');
+        }
+    });
+}
+
+
+function unRotateArrow(i) {
+    chevron.forEach(function (arrow) {
+        if (arrow.getAttribute('data-ind') === i) {
+            arrow.classList.remove('rotated-arrow');
+        }
+    })
+}
+
+
+
+// back to top button functionality
 
 window.onscroll = function() {
     scrollFunction();
@@ -28,6 +95,10 @@ function scrollToTop() {
         behavior: 'smooth' // Smooth scroll to top
     }); 
 }
+
+
+
+// header navigation icon functionality
 
 menuBar.addEventListener('click', function () {
     if (menu.style.display === 'flex') {
